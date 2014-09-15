2c74117b4f22baf28b15b2fe166a3ba33ca73597
Stefan Marx <smarx@marxworld.net>
2014-09-16 00:15:08 +0200

    Added Functions

---
+++ ADRestDataConnection/src/de/appdynamics/ace/report/jasperreports/studio/functions/PathFunctions.java
@@ -0,0 +1,33 @@
+package de.appdynamics.ace.report.jasperreports.studio.functions;
+
+import net.sf.jasperreports.functions.annotations.Function;
+import net.sf.jasperreports.functions.annotations.FunctionCategories;
+import net.sf.jasperreports.functions.annotations.FunctionParameter;
+import net.sf.jasperreports.functions.annotations.FunctionParameters;
+
+@FunctionCategories ({PathCategory.class})
+public final class PathFunctions {
+	// ===================== DAY function ===================== //
+	  /**
+	   * 
+	   * Returns the day of a given date. Date object can be a String, long value (milliseconds) or Date instance itself.
+	   */
+	  @Function("PATH_SEGMENT")
+	  @FunctionParameters({
+		  @FunctionParameter("string"),
+		  @FunctionParameter("index")
+		    })
+	  public static String PathSegment(String path,Integer index){
+		 String[] pathSegments = path.split("\\|");
+		 if (index >= pathSegments.length) index = pathSegments.length-1;
+		 
+		 if (index < 0) {
+			 index = pathSegments.length-(index*-1);
+		 }
+		 
+		 if (index<0) index=0;
+				 
+		 
+	    return pathSegments[index];
+	  }
+}
